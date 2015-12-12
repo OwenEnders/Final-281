b4cbd19e21041bcc1c15413c974f603c538648ed
Owen <owen.enders@mymail.champlain.edu>
2015-12-11 21:37:11 -0500

    made constructors for itemset in classes.h

--- Apriori/classes.cpp
+++ Apriori/classes.cpp
@@ -3,7 +3,7 @@
 
 
 
-void ItemsetHolder::buildItemsets(int** buildDatabase)
+void ItemsetHolder::buildItemsets(int** database)
 {
 	//for single item itemsets, fill in linked list with the item of the itemset
 	//call the determineSupport function
@@ -14,12 +14,12 @@ void ItemsetHolder::buildItemsets(int** buildDatabase)
 	for (int i = 0; i < 100000; i++)  
 		for (int j = 1; j < 50; j++)
 		{
-			Itemset::items.insert(database[i][j]);
+			items.insert(database[i][j]);
 
 		}
 }
 
-void ItemsetHolder::pruneItemsets(, int support)
+void ItemsetHolder::pruneItemsets( int support)
 {
 	//go through array, if an itemsets support isnt great enough, destroy the itemset.
 	//every time function is called, start off where it last stopped, instead of at the beginning of the array
